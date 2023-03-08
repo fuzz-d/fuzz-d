2b46e9d5260d77ab2338b2dad948d16266f094b8
Alex Usher <alex.usher19@imperial.ac.uk>
2023-03-08 19:48:06 +0000

    Loosely sketches visitors for top level nodes

--- app/src/main/kotlin/fuzzd/Main.kt
+++ app/src/main/kotlin/fuzzd/Main.kt
@@ -1,14 +1,22 @@
 package fuzzd
 
+import dafnyLexer
+import dafnyParser
 import fuzzd.generator.Generator
+import fuzzd.generator.ast.DafnyAST
 import fuzzd.generator.selection.SelectionManager
 import fuzzd.logging.Logger
 import fuzzd.logging.OutputWriter
+import fuzzd.recondition.DafnyVisitor
 import fuzzd.recondition.Reconditioner
 import fuzzd.utils.WRAPPER_FUNCTIONS
 import fuzzd.validator.OutputValidator
+import org.antlr.v4.runtime.CharStreams
+import org.antlr.v4.runtime.CommonTokenStream
+import java.io.File
 import java.util.UUID
 import kotlin.random.Random
+import kotlin.system.exitProcess
 
 class Main(private val path: String) {
     private val validator = OutputValidator()
@@ -25,7 +33,15 @@ class Main(private val path: String) {
 
         // generate program
         try {
-            val ast = generator.generate()
+            val ast = if (false /* TODO() */) {
+                val input = File("../test.dfy").inputStream()
+                val charStream = CharStreams.fromStream(input)
+                val tokens = CommonTokenStream(dafnyLexer(charStream))
+
+                DafnyVisitor().visitProgram(dafnyParser(tokens).program())
+            } else {
+                generator.generate()
+            }
 
             val reconditioner = Reconditioner()
             val reconditionedAST = reconditioner.recondition(ast)
--- app/src/main/kotlin/fuzzd/recondition/DafnyVisitor.kt
+++ app/src/main/kotlin/fuzzd/recondition/DafnyVisitor.kt
@@ -2,42 +2,156 @@ package fuzzd.recondition
 
 import dafnyBaseVisitor
 import dafnyParser.ClassDeclContext
+import dafnyParser.ClassMemberDeclContext
+import dafnyParser.ExpressionContext
+import dafnyParser.FieldDeclContext
 import dafnyParser.FunctionDeclContext
+import dafnyParser.FunctionSignatureDeclContext
+import dafnyParser.IdentifierContext
+import dafnyParser.IdentifierTypeContext
 import dafnyParser.MethodDeclContext
+import dafnyParser.MethodSignatureDeclContext
+import dafnyParser.ParametersContext
 import dafnyParser.ProgramContext
+import dafnyParser.StatementContext
 import dafnyParser.TopDeclContext
 import dafnyParser.TopDeclMemberContext
 import dafnyParser.TraitDeclContext
+import dafnyParser.TypeContext
 import fuzzd.generator.ast.ASTElement
 import fuzzd.generator.ast.ClassAST
 import fuzzd.generator.ast.DafnyAST
+import fuzzd.generator.ast.ExpressionAST
+import fuzzd.generator.ast.ExpressionAST.IdentifierAST
 import fuzzd.generator.ast.FunctionMethodAST
+import fuzzd.generator.ast.FunctionMethodSignatureAST
+import fuzzd.generator.ast.MethodAST
+import fuzzd.generator.ast.MethodSignatureAST
+import fuzzd.generator.ast.SequenceAST
+import fuzzd.generator.ast.StatementAST
 import fuzzd.generator.ast.TopLevelAST
 import fuzzd.generator.ast.TraitAST
+import fuzzd.generator.ast.Type
+import fuzzd.utils.unionAll
 
 class DafnyVisitor : dafnyBaseVisitor<ASTElement>() {
+    val traits = mutableMapOf<String, TraitAST>() // get trait object by name
+
     override fun visitProgram(ctx: ProgramContext): DafnyAST =
         DafnyAST(
             (ctx.topDecl()?.map { topDeclCtx -> visitTopDecl(topDeclCtx) }) ?: listOf()
         )
 
-    override fun visitTopDecl(ctx: TopDeclContext): TopLevelAST = visit(ctx) as TopLevelAST
+    override fun visitTopDecl(ctx: TopDeclContext): TopLevelAST = super.visitTopDecl(ctx) as TopLevelAST
 
     override fun visitClassDecl(ctx: ClassDeclContext): ClassAST {
-        TODO()
+        val name = visitIdentifierName(ctx.identifier(0))
+
+        val extends = ctx.identifier().slice(1 until ctx.identifier().size)
+            .map { identifierCtx ->
+                identifierCtx.IDENTIFIER().toString()
+            }
+            .map { identifierStr -> traits[identifierStr]!! }
+            .toSet()
+
+        val fields = mutableSetOf<IdentifierAST>()
+        val functionMethods = mutableSetOf<FunctionMethodAST>()
+        val methods = mutableSetOf<MethodAST>()
+
+        ctx.classMemberDecl().forEach { classMemberDeclCtx ->
+            when (val astNode = super.visitClassMemberDecl(classMemberDeclCtx)) {
+                is IdentifierAST -> fields.add(astNode)
+                is FunctionMethodAST -> functionMethods.add(astNode)
+                is MethodAST -> methods.add(astNode)
+                else -> {} // do nothing
+            }
+        }
+
+        val inheritedFields = extends.map { it.fields() }.unionAll()
+
+        return ClassAST(name, extends, functionMethods, methods, fields, inheritedFields)
     }
 
     override fun visitTraitDecl(ctx: TraitDeclContext): TraitAST {
-        TODO()
+        val name = visitIdentifierName(ctx.identifier(0))
+
+        val extends = ctx.identifier().slice(1 until ctx.identifier().size)
+            .map { identifierCtx ->
+                identifierCtx.IDENTIFIER().toString()
+            }
+            .map { identifierStr -> traits[identifierStr]!! }
+            .toSet()
+
+        val fields = mutableSetOf<IdentifierAST>()
+        val functionMethods = mutableSetOf<FunctionMethodSignatureAST>()
+        val methods = mutableSetOf<MethodSignatureAST>()
+
+        ctx.traitMemberDecl().forEach { traitMemberDeclCtx ->
+            when (val astNode = super.visitTraitMemberDecl(traitMemberDeclCtx)) {
+                is IdentifierAST -> fields.add(astNode)
+                is FunctionMethodSignatureAST -> functionMethods.add(astNode)
+                is MethodSignatureAST -> methods.add(astNode)
+            }
+        }
+
+        val trait = TraitAST(name, extends, functionMethods, methods, fields)
+
+        traits[name] = trait
+
+        return trait
     }
 
-    override fun visitTopDeclMember(ctx: TopDeclMemberContext): TopLevelAST = visit(ctx) as TopLevelAST
+    override fun visitTopDeclMember(ctx: TopDeclMemberContext): TopLevelAST =
+        super.visitTopDeclMember(ctx) as TopLevelAST
+
+    override fun visitFunctionSignatureDecl(ctx: FunctionSignatureDeclContext): FunctionMethodSignatureAST {
+        val name = visitIdentifierName(ctx.identifier())
+
+        val params = visitParametersList(ctx.parameters())
+        val returnType = visitType(ctx.type())
+
+        return FunctionMethodSignatureAST(name, returnType, params)
+    }
+
+    override fun visitFieldDecl(ctx: FieldDeclContext): IdentifierAST = visitIdentifierType(ctx.identifierType())
 
     override fun visitFunctionDecl(ctx: FunctionDeclContext): FunctionMethodAST {
-        TODO()
+        val signature = visitFunctionSignatureDecl(ctx.functionSignatureDecl())
+        val body = visitExpression(ctx.expression())
+
+        return FunctionMethodAST(signature, body)
     }
 
-    override fun visitMethodDecl(ctx: MethodDeclContext?): ASTElement {
-        TODO()
+    override fun visitMethodDecl(ctx: MethodDeclContext): ASTElement {
+        val signature = visitMethodSignatureDecl(ctx.methodSignatureDecl())
+        val body = SequenceAST(ctx.statement().map(this::visitStatement))
+
+        val method = MethodAST(signature)
+        method.setBody(body)
+
+        return method
     }
+
+    override fun visitMethodSignatureDecl(ctx: MethodSignatureDeclContext): MethodSignatureAST {
+        val name = visitIdentifierName(ctx.identifier())
+
+        val params = visitParametersList(ctx.parameters(0))
+        val returns = if (ctx.parameters().size > 1) visitParametersList(ctx.parameters(1)) else emptyList()
+
+        return MethodSignatureAST(name, params, returns)
+    }
+
+    private fun visitParametersList(ctx: ParametersContext): List<IdentifierAST> =
+        ctx.identifierType().map { identifierTypeCtx -> visitIdentifierType(identifierTypeCtx) }
+
+    override fun visitIdentifierType(ctx: IdentifierTypeContext): IdentifierAST =
+        IdentifierAST(visitIdentifierName(ctx.identifier()), visitType(ctx.type()))
+
+    override fun visitStatement(ctx: StatementContext): StatementAST = TODO()
+
+    override fun visitExpression(ctx: ExpressionContext): ExpressionAST = TODO()
+
+    override fun visitType(ctx: TypeContext): Type = TODO()
+
+    private fun visitIdentifierName(identifierCtx: IdentifierContext): String = identifierCtx.IDENTIFIER().toString()
 }
