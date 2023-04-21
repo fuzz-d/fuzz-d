c315ec529ce1f3b59167238beb4c51cb131d6334
Alex Usher <alex.usher19@imperial.ac.uk>
2023-04-21 13:32:39 +0100

    Adds custom output for java compiler crash

--- app/src/main/kotlin/fuzzd/validator/ValidationResult.kt
+++ app/src/main/kotlin/fuzzd/validator/ValidationResult.kt
@@ -2,6 +2,7 @@ package fuzzd.validator
 
 import fuzzd.utils.indent
 import fuzzd.validator.executor.execution_handler.ExecutionHandler
+import fuzzd.validator.executor.execution_handler.JavaExecutionHandler
 
 class ValidationResult(handlers: List<ExecutionHandler>, val targetOutput: String?) {
     private val succeededCompile: List<ExecutionHandler>
@@ -45,7 +46,8 @@ class ValidationResult(handlers: List<ExecutionHandler>, val targetOutput: Strin
         sb.appendLine("Target Output: $targetOutput")
         succeededExecute.forEach { h -> sb.append("${h.getCompileTarget()}:\n${indent(h.executeResult())}\n") }
 
-        sb.appendLine("Compiler crash: ${failedCompile.isNotEmpty()}")
+        sb.appendLine("Java Compiler crash: ${failedCompile.filterIsInstance<JavaExecutionHandler>().isNotEmpty()}")
+        sb.appendLine("Compiler crash: ${failedCompile.filter { it !is JavaExecutionHandler }.isNotEmpty()}")
         sb.appendLine("Execute crash: ${failedExecute.isNotEmpty()}")
         sb.appendLine("Different output: ${differentOutput()}")
 
