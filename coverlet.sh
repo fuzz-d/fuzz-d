58e5001772075560a003e4469bd402ce8b2e99de
Alex Usher <alex.usher19@imperial.ac.uk>
2023-04-27 15:47:30 +0100

    Sets up coverage script to compile over all backends and generate report

--- coverlet.sh
+++ coverlet.sh
@@ -1,1 +1,14 @@
-cd $DAFNYBIN && coverlet . --target "dotnet" --targetargs "Dafny.dll /noVerify /compile:2 /out:/Users/alex/fyp/fuzz-d/output /Users/alex/fyp/fuzz-d/output/js-timeout/main.dfy " -f cobertura -o /Users/alex/fyp/fuzz-d/output/js-timeout/coverage.cobertura.xml
+REPORT=coverage/coverage.cobertura.xml
+OUTPUT=coverage/result.json
+for dir in ./coverage_testcases/*; do
+echo "cs"
+coverlet $DAFNYBIN --target "dotnet" --targetargs "$DAFNYBIN/Dafny.dll /noVerify /compile:2 /compileTarget:cs $dir/main.dfy" --merge-with $OUTPUT -f json
+echo "java"
+coverlet $DAFNYBIN --target "dotnet" --targetargs "$DAFNYBIN/Dafny.dll /noVerify /compile:2 /compileTarget:java $dir/main.dfy" --merge-with $OUTPUT -f json
+echo "py"
+coverlet $DAFNYBIN --target "dotnet" --targetargs "$DAFNYBIN/Dafny.dll /noVerify /compile:2 /compileTarget:py $dir/main.dfy" --merge-with $OUTPUT -f json
+echo "go"
+coverlet $DAFNYBIN --target "dotnet" --targetargs "$DAFNYBIN/Dafny.dll /noVerify /compile:2 /compileTarget:go $dir/main.dfy" --merge-with $OUTPUT -f json
+echo "js"
+coverlet $DAFNYBIN --target "dotnet" --targetargs "$DAFNYBIN/Dafny.dll /noVerify /compile:2 /compileTarget:js $dir/main.dfy" --merge-with $OUTPUT -f cobertura -o $REPORT
+; done
