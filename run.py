d58a81d6d97b2c1abab6a93b77e4795047c56677
Alex Usher <alex.usher19@imperial.ac.uk>
2023-05-04 14:28:32 +0100

    progress on coverage runner

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env python3
 
 import time
 import os
@@ -12,7 +12,6 @@ def getErrorDirectories():
     execute_crash_dirs = []
     different_output_dirs = []
     timeout_dirs = []
-    success_dirs = []
     directory = 'output'
     filename = 'fuzz-d.log'
     for dir_name in os.listdir(directory):
@@ -24,29 +23,20 @@ def getErrorDirectories():
             continue
 
         with open(f) as open_file:
-            flag = False
             file_contents = open_file.read()
             if 'resolution/type errors detected' in file_contents:
-                flag = True
                 resolution_dirs += [str(dir_name)]
             if 'Java crash: true' in file_contents:
-                flag = True
                 java_crash_dirs += [str(dir_name)]
             if 'Compiler crash: true' in file_contents:
-                flag = True
                 compiler_crash_dirs += [str(dir_name)]
             if 'Execute crash: true' in file_contents:
-                flag = True
                 execute_crash_dirs += [str(dir_name)]
             if 'Different output: true' in file_contents:
-                flag = True
                 different_output_dirs += [str(dir_name)]
             if 'Exit code: 2' in file_contents or 'Exit code: 124' in file_contents:
-                flag = True
                 timeout_dirs += [str(dir_name)]
-            if not flag:
-                success_dirs += [str(dir_name)]
-    return resolution_dirs, java_crash_dirs, compiler_crash_dirs, execute_crash_dirs, different_output_dirs, timeout_dirs, success_dirs
+    return resolution_dirs, java_crash_dirs, compiler_crash_dirs, execute_crash_dirs, different_output_dirs, timeout_dirs
 
 def copyDirectories(directories, directory):
     for dir_name in directories:
@@ -55,20 +45,21 @@ def copyDirectories(directories, directory):
         copy_tree(dir_path, res_path)
 
 if __name__ == '__main__':
-    timeout_start = time.time()    
+    timeout_start = time.time()
     timeout_secs = 60 * 60 * 8 # 8 hours
+    base_command = "timeout 60 java -jar /home/alex/fuzz-d/app/build/libs/app.jar fuzz"
     while time.time() < timeout_start + timeout_secs:
         for i in range(10):
             print(time.time())
-            os.system("timeout 60 java -jar /home/alex/fuzz-d/app/build/libs/app.jar fuzz")
-        
-        resolution_dirs, java_crash_dirs, compiler_crash_dirs, execute_crash_dirs, different_output_dirs, timeout_dirs, success_dirs = getErrorDirectories()
+            os.system(base_command)
+
+        resolution_dirs, java_crash_dirs, compiler_crash_dirs, execute_crash_dirs, different_output_dirs, timeout_dirs = getErrorDirectories()
         copyDirectories(resolution_dirs, os.path.join('result', 'resolver'))
         copyDirectories(java_crash_dirs, os.path.join('result', 'java_crash'))
         copyDirectories(compiler_crash_dirs, os.path.join('result', 'compiler_crash'))
         copyDirectories(execute_crash_dirs, os.path.join('result', 'execute_crash'))
         copyDirectories(different_output_dirs, os.path.join('result', 'different_output'))
         copyDirectories(timeout_dirs, os.path.join('result', 'timeout'))
-        copyDirectories(success_dirs, os.path.join('result', 'success'))
-    
+
         os.system('rm -rf output/*')
+        os.system('rm -rf success/*')
