b37573bb0aa88008b601317ba36a1e73f16bf3f7
Alex Usher <alex.usher19@imperial.ac.uk>
2023-05-25 20:50:22 +0100

    Updating coverage runner to final coverage setup

--- coverage_runner.py
+++ coverage_runner.py
@@ -43,33 +43,34 @@ def run_coverage(program, coverage_report_json, coverage_report_cobertura):
 
 # usage: coverage_runner.py RUN_NAME OUTPUT_DIR 
 if __name__ == '__main__':
-    run_name = sys.argv[1]
-    runner = FuzzdRunner() if run_name == "fuzzd" else XDSmithRunner()
-    output_dir = pathlib.Path(sys.argv[2]) / "results" / run_name
-    coverage_dir = output_dir / "coverage"
+    for i in range(1, 3):
+        run_name = sys.argv[1]
+        runner = FuzzdRunner() if run_name == "fuzzd" else XDSmithRunner()
+        output_dir = pathlib.Path(sys.argv[2]) / "results" / f"{run_name}{i}"
+        coverage_dir = output_dir / "coverage"
 
-    output_dir.mkdir(parents=True, exist_ok=True)
-    coverage_dir.mkdir(parents=True, exist_ok=True)
+        output_dir.mkdir(parents=True, exist_ok=True)
+        coverage_dir.mkdir(parents=True, exist_ok=True)
 
-    output_log = output_dir / "run.log" 
-    coverage_report_json = coverage_dir / "coverage.json"
-    coverage_report_cobertura = coverage_dir / "coverage.cobertura.xml"
+        output_log = output_dir / "run.log" 
+        coverage_report_json = coverage_dir / "coverage.json"
+        coverage_report_cobertura = coverage_dir / "coverage.cobertura.xml"
 
-    output_log.write_text(f'Git commit: ')
-    os.system(f'/bin/bash -c "git rev-parse HEAD >> {output_log}"')
+        output_log.write_text(f'Git commit: ')
+        os.system(f'/bin/bash -c "git rev-parse HEAD >> {output_log}"')
 
-    start_time = time.time()
-    timeout_secs = 60 * 60 * 24
-    checkpoints = [1, 4, 8, 12, 16, 20, 24]
-    checkpoints_saved = [False] * len(checkpoints)
-    while (time.time() < start_time + timeout_secs):
-        for (i, checkpoint) in enumerate(checkpoints):
-            if (not checkpoints_saved[i] and time.time() >= start_time + checkpoint * 60 * 60):
-                coverage_checkpoint_dir = output_dir / f'coverage_{checkpoint}'
-                copy_tree(str(coverage_dir), str(coverage_checkpoint_dir))
-                checkpoints_saved[i] = True
+        start_time = time.time()
+        timeout_secs = 60 * 60 * 12
+        checkpoints = [2, 4, 6, 8, 10, 12]
+        checkpoints_saved = [False] * len(checkpoints)
+        while (time.time() < start_time + timeout_secs):
+            for (i, checkpoint) in enumerate(checkpoints):
+                if (not checkpoints_saved[i] and time.time() >= start_time + checkpoint * 60 * 60):
+                    coverage_checkpoint_dir = output_dir / f'coverage_{checkpoint}'
+                    copy_tree(str(coverage_dir), str(coverage_checkpoint_dir))
+                    checkpoints_saved[i] = True
 
-        return_code, generated_file = generate_program(output_log, runner)
-        if return_code == 0:
-            run_coverage(generated_file, coverage_report_json, coverage_report_cobertura)
+            return_code, generated_file = generate_program(output_log, runner)
+            if return_code == 0:
+                run_coverage(generated_file, coverage_report_json, coverage_report_cobertura)
 
