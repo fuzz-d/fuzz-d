6f63fdea131f19e47e05bd8ab50f450ec84b8190
Ubuntu <ubuntu@ip-172-31-46-233.us-west-2.compute.internal>
2023-10-20 11:34:09 +0000

    Fixes coverlet script

--- coverlet.sh
+++ coverlet.sh
@@ -1,7 +1,7 @@
 FILE=$1
 OUTPUT=$2
 coverlet $DAFNYBIN --target "dotnet" --targetargs "$DAFNYBIN/Dafny.dll /noVerify /compile:2 /compileTarget:cs $FILE" --merge-with $OUTPUT.json -f json -f cobertura -o $OUTPUT
-coverlet $DAFNYBIN --target "dotnet" --targetargs "$DAFNYBIN/Dafny.dll /noVerify /compile:2 /compileTarget:java $FILE" --merge-with $OUTPUT -f json -f cobertura -o $OUTPUT
-coverlet $DAFNYBIN --target "dotnet" --targetargs "$DAFNYBIN/Dafny.dll /noVerify /compile:2 /compileTarget:py $FILE" --merge-with $OUTPUT -f json -f cobertura -o $OUTPUT
-coverlet $DAFNYBIN --target "dotnet" --targetargs "$DAFNYBIN/Dafny.dll /noVerify /compile:2 /compileTarget:go $FILE" --merge-with $OUTPUT -f json -f cobertura -o $OUTPUT
-coverlet $DAFNYBIN --target "dotnet" --targetargs "$DAFNYBIN/Dafny.dll /noVerify /compile:2 /compileTarget:js $FILE" --merge-with $OUTPUT -f json -f cobertura -o $OUTPUT
+coverlet $DAFNYBIN --target "dotnet" --targetargs "$DAFNYBIN/Dafny.dll /noVerify /compile:2 /compileTarget:java $FILE" --merge-with $OUTPUT.json -f json -f cobertura -o $OUTPUT
+coverlet $DAFNYBIN --target "dotnet" --targetargs "$DAFNYBIN/Dafny.dll /noVerify /compile:2 /compileTarget:py $FILE" --merge-with $OUTPUT.json -f json -f cobertura -o $OUTPUT
+coverlet $DAFNYBIN --target "dotnet" --targetargs "$DAFNYBIN/Dafny.dll /noVerify /compile:2 /compileTarget:go $FILE" --merge-with $OUTPUT.json -f json -f cobertura -o $OUTPUT
+coverlet $DAFNYBIN --target "dotnet" --targetargs "$DAFNYBIN/Dafny.dll /noVerify /compile:2 /compileTarget:js $FILE" --merge-with $OUTPUT.json -f json -f cobertura -o $OUTPUT
